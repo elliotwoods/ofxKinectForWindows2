dc79fee0d5584782d792b98684c935014922dd10
Elie Zananiri <prisonerjohn@gmail.com>
2015-03-12 10:16:06 -0400

    Added GL3 shaders to bodyIndex example.

--- exampleBodyIndex/src/main.cpp
+++ exampleBodyIndex/src/main.cpp
@@ -3,6 +3,15 @@
 
 //--------------------------------------------------------------
 int main(){
+#ifdef USE_PROGRAMMABLE_PIPELINE
+	ofGLWindowSettings settings;
+	settings.setGLVersion(4,3);
+	settings.width = 1024;
+	settings.height = 768;
+	ofCreateWindow(settings);
+#else
 	ofSetupOpenGL(1024, 768, OF_WINDOW);
+#endif
+
 	ofRunApp(new ofApp());
 }
