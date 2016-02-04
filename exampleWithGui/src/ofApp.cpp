d59640115f8ae8c1b53b3a35bd7e70eeb0007334
Elliot Woods <elliot@kimchiandchips.com>
2016-02-04 16:55:25 +0900

    update Gui example also scale depth view

--- exampleWithGui/src/ofApp.cpp
+++ exampleWithGui/src/ofApp.cpp
@@ -14,10 +14,11 @@ void ofApp::setup(){
 	//setup a gui panel for every kinect source
 	auto sources = kinect.getSources();
 	for(auto source : sources) {
-		auto drawingSource = dynamic_pointer_cast<ofBaseDraws>(source);
-		ofxCvGui::PanelPtr panel;
-		if (drawingSource) {
-			panel = gui.add(*drawingSource, source->getTypeName());
+		auto sourceWithTexture = dynamic_pointer_cast<ofBaseHasTexture>(source);
+		if (sourceWithTexture) {
+			auto panel = gui.add(sourceWithTexture->getTexture(), source->getTypeName());
+
+			//if it's the colour panel, let's do something special by writing some info on top
 			auto colorSource = dynamic_pointer_cast<ofxKFW2::Source::Color>(source);
 			if (colorSource) {
 				panel->onDraw += [colorSource] (ofxCvGui::DrawArguments &) {
@@ -28,6 +29,14 @@ void ofApp::setup(){
 					message << "Gamma : " << colorSource->getGamma() << endl;
 					ofxCvGui::Utils::drawText(message.str(), 20, 60);
 				};
+			}
+
+			//if it's the depth panel, set some scaling
+			auto depthSource = dynamic_pointer_cast<ofxKFW2::Source::Depth>(source);
+			if (depthSource) {
+				auto style = panel->getStyle();
+				style.rangeMaximum = 0.25f;
+				//panel->setStyle(style);
 			}
 		}
 	}
@@ -40,7 +49,7 @@ void ofApp::setup(){
 	//if we press the 'c' key on the World panel, then toggle the camera's cursor. This works best when you fullscreen that panel
 	worldView->onKeyboard += [this, worldView] (ofxCvGui::KeyboardArguments & args) {
 		if (args.action == ofxCvGui::KeyboardArguments::Action::Pressed && args.key =='c') {
-			worldView->getCamera().toggleCursorDraw();
+			worldView->getCamera().toggleCursorDrawEnabled();
 		}
 	};
 }
