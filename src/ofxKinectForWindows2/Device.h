1a2a01f831792704aa01aa17596fad605bf6224f
Elie Zananiri <prisonerjohn@gmail.com>
2016-05-16 18:07:26 -0400

    Added methods to release Sources after they've been opened.

--- src/ofxKinectForWindows2/Device.h
+++ src/ofxKinectForWindows2/Device.h
@@ -31,6 +31,16 @@ namespace ofxKinectForWindows2 {
 		shared_ptr<Source::BodyIndex> initBodyIndexSource();
 		shared_ptr<Source::Body> initBodySource();
 
+        template<typename SourceType>
+        bool releaseSource();
+
+        bool releaseDepthSource();
+        bool releaseColorSource();
+        bool releaseInfraredSource();
+        bool releaseLongExposureInfraredSource();
+        bool releaseBodyIndexSource();
+        bool releaseBodySource();
+
 		void update();
 		bool isFrameNew() const;
 
