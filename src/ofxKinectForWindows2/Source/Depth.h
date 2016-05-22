995ccc122decb8d80b226c70a09a0209c58b5425
Elliot Woods <elliot@kimchiandchips.com>
2016-05-22 17:37:42 +0900

    bring @fralken's code back in for getting color resolution, but guard it from causing Depth init to fail (probably this is enitirely unnecessary.)

--- src/ofxKinectForWindows2/Source/Depth.h
+++ src/ofxKinectForWindows2/Source/Depth.h
@@ -43,9 +43,9 @@ namespace ofxKinectForWindows2 {
 
 			ICoordinateMapper * coordinateMapper;
 
-			const int colorFrameWidth = 1920;
-			const int colorFrameHeight = 1080;
-			const int colorFrameSize = colorFrameWidth * colorFrameHeight;
+			int colorFrameWidth = 1920;
+			int colorFrameHeight = 1080;
+			int colorFrameSize = colorFrameWidth * colorFrameHeight;
 		};
 	}
 }
